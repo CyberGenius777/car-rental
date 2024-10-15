f6bd14c58fdf9ee8e613230ae4dbe5dc95e5f993
CyberGenius <cybergenius777@gmail.com>
2024-10-15 22:07:49 +0300

    prettier added

---
+++ .prettierrc.js
@@ -0,0 +1,41 @@
+/** @type {import('prettier').Config} */
+
+module.exports = {
+  arrowParens: "always",
+  printWidth: 120,
+  jsxSingleQuote: true,
+  jsxBracketSameLine: false,
+  endOfLine: "lf",
+  semi: false,
+  singleQuote: true,
+  tabWidth: 4,
+  useTabs: false,
+  trailingComma: "all",
+  overrides: [
+    {
+      files: ["*.js", "*.ts", "*.jsx", "*.tsx"],
+      options: {
+        parser: "typescript",
+      },
+    },
+    {
+      files: ["*.md"],
+      options: {
+        parser: "markdown",
+      },
+    },
+    {
+      files: ["*.json", ".prettierrc"],
+      options: {
+        parser: "json",
+      },
+    },
+    {
+      files: ["*.yml", "*.yaml"],
+      options: {
+        tabWidth: 2,
+        singleQuote: false,
+      },
+    },
+  ],
+};
